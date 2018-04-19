0ff5bd4ca5165fbc239ba1ee977de2f8539f7779
Shawn Wildermuth <shawn@wildermuth.com>
2018-04-18 21:45:22 -0400

    Lab 2

---
+++ Labs/Lab2/before/Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheStore.Data;
+using TheStore.Data.Entities;
+
+namespace TheStore.Controllers.Api
+{
+  [Authorize]
+  [Route("api/[controller]")]
+  public class CustomerController : Controller
+  {
+    private readonly IStoreRepository _repository;
+
+    public CustomerController(IStoreRepository repository)
+    {
+      _repository = repository;
+    }
+
+    [HttpGet()]
+    public IActionResult Get()
+    {
+      var cust = _repository.GetCustomer(User.Identity.Name);
+      if (cust == null) return NotFound();
+      return Ok(cust);
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> Post([FromBody]Customer model)
+    {
+      if (ModelState.IsValid)
+      {
+        if (_repository.GetCustomer(User.Identity.Name) != null)
+        {
+          return BadRequest("Customer Exists. Try updating customer instead.");
+        }
+
+        model.Username = User.Identity.Name;
+
+        _repository.Add(model);
+        if (await _repository.SaveAsync())
+        {
+          return Created($"api/customer", model);
+        }
+      }
+
+      return BadRequest("Failed to create new customer");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete()
+    {
+      var customer = _repository.GetCustomer(User.Identity.Name);
+      if (customer != null)
+      {
+        _repository.Delete(customer);
+        if (await _repository.SaveAsync())
+        {
+          return Ok();
+        }
+      }
+
+      return BadRequest();
+    }
+  }
+}
---
+++ Labs/Lab2/before/wwwroot/js/index.js
@@ -0,0 +1,4 @@
+﻿// index.js
+new Vue({
+  el: "#index-view"
+});
