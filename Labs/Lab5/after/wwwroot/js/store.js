aa9261a1b456059c3174dd449948bc553839820e
Shawn Wildermuth <shawn@wildermuth.com>
2018-05-11 19:15:19 -0400

    Lab 5

---
+++ Labs/Lab5/after/wwwroot/js/store.js
@@ -0,0 +1,33 @@
+﻿// store.js
+let store = new Vuex.Store({
+  state: {
+    cart: [],
+    products: [],
+  },
+  mutations: {
+    setProducts: function (state, newCatalog) {
+      state.products = newCatalog;
+    },
+    addProductToCart: function(state, product) {
+      state.cart.push(product);
+    },
+    clearCart: function (state) {
+      state.cart = [];
+    }
+  },
+  actions: {
+    loadCatalog: function (context) {
+      return new Promise(function (resolve, reject) {
+        axios.get("/api/products")
+          .then(function (res) {
+            context.commit("setProducts", res.data.results);
+            resolve();
+          })
+          .catch(function () {
+            reject();
+          });
+      });
+    }
+  },
+  getters: {}
+});
---
+++ Labs/Lab5/before/wwwroot/js/cart.js
@@ -0,0 +1,24 @@
+﻿// cart.js
+Vue.component("the-cart", {
+  template: `
+<div>
+  <h3>Shopping Cart</h3>
+  <button class='btn btn-info' v-on:click="emptyCart">Empty Cart</button>
+  <div v-for="i in items">
+    <div>{{ i.name }} - {{ i.listPrice }}</div>
+    <hr/>
+  </div>
+</div>
+`,
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    emptyCart: function () {
+      this.$emit("empty-cart");
+    }
+  }
+});
