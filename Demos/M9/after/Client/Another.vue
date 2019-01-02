b3ef6935d9ffdb4489b9ed3ecc5f50d38b2a475c
Shawn Wildermuth <shawn@wildermuth.com>
2019-01-02 16:44:18 -0500

    Module 9 Added

---
+++ Demos/M9/after/Client/Another.vue
@@ -0,0 +1,6 @@
+﻿<template>
+  <div>
+    <h3>This is for another page, probably contact</h3>
+    <p>This is even more text</p>
+  </div>
+</template>
---
+++ Demos/M9/after/vue.config.js
@@ -0,0 +1,10 @@
+﻿// vue.config.js
+module.exports = {
+  outputDir: "./wwwroot/client",
+  pluginOptions: {
+    sourceDir: "Client"
+  },
+  pages: {
+    another: "Client/another.js"
+  }
+}
